7dd75a017f17f871adca951b6ad4c157c1a13f76
fengnanyue <fyue@cmu.edu>
2016-10-26 16:31:30 -0700

    update project

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,11 +4,38 @@ import play.*;
 import play.mvc.*;
 
 import views.html.*;
+import play.data.DynamicForm;
+import play.data.Form;
 
 public class Application extends Controller {
 
     public Result index() {
         return ok(index.render());
+    }
+    
+    public Result getName() {
+        return ok(index.render());
+    }
+    public Result loginSubmit() {
+
+        DynamicForm form = Form.form().bindFromRequest();
+
+        if (form.data().size() != 2) {
+            return badRequest("Bad Request!!!!!!");
+        } else {
+            String response = "Username: " + form.get("userName") + " Password: " + form.get("password");
+            return ok(response);
+        }
+    }
+    
+    public Result registerUser() {
+        DynamicForm form = Form.form().bindFromRequest();
+        if (form.data().size() != 2) {
+            return badRequest("Bad Request!!!!!!");
+        } else {
+            String response = "Register the user: " + "Username " + form.get("userName") + " Password " + form.get("password");
+            return ok(response);
+        }
     }
 
 }
