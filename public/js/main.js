e52c8765a548cd05216ee6266d8452dcee752fb7
Angela Gao <angelagao@AngelatekiMacBook-Pro.local>
2016-10-27 13:09:04 -0700

    modify nav and header

--- public/js/main.js
+++ public/js/main.js
@@ -25,9 +25,9 @@ require(['angular', './controllers', './directives', './filters', './services', 
 
     angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ngRoute']).
       config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: controllers.MyCtrl1});
-        $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: controllers.MyCtrl2});
-        $routeProvider.otherwise({redirectTo: '/view1'});
+        $routeProvider.when('/register', {templateUrl: 'partials/register.html', controller: controllers.MyCtrl1});
+        $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: controllers.MyCtrl2});
+        $routeProvider.otherwise({redirectTo: '/register'});
       }]);
 
     angular.bootstrap(document, ['myApp']);
