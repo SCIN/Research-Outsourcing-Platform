6c04654884843662c8c52913338baa44304153d3
Angela Gao <angelagao@AngelatekiMacBook-Pro.local>
2016-11-15 18:18:17 -0800

    modify publish project controller with model and POST request

--- public/js/controllers/publishProjectController.js
+++ public/js/controllers/publishProjectController.js
@@ -6,61 +6,34 @@ define([], function() {
         $scope.userName = $rootScope.user.userName;
         $scope.role = $rootScope.user.role;
         // console.log($rootScope.user.role);
-        $scope.providerinfo = {
-            credential: "",
-            researchAreas:"",
-            publications:"",
-            professionalServices:""
+        $scope.projectInfo = {
+            projectName: "mock",
+            projectDescription:"mock",
+            requiredExpertise:"mock",
+            begintime:"mock",
+            endtime: "mock",
+            price:"mock",
+            status:"new"
         };
 
-        // $scope.getUserInfo = function() {
-        //     $http({
-        //         method : 'GET',
-        //         url : '/users/'+ $scope.userName + '/userInfo',
-        //     }).success(function(data, status, headers, config) {
-        //             $scope.loginResult = data;
-        //             console.log(data);
-        //             $location.path('/dashboard');
-        //         }
-        //
-        //     ).error(function (data, status, headers, config) {
-        //         console.log(data);
-        //     });
-        // }
-
-        $scope.getProviderInfo = function() {
-            $http({
-                method : 'GET',
-                url : '/users/'+ $scope.userName + '/providerinfo'
-            }).success(function(data, status, headers, config) {
-                    $scope.providerinfo = data;
-                    // console.log(data);
-                }
-
-            ).error(function (data, status, headers, config) {
-                // console.log(data);
-            });
+        $scope.updateExpertise = function() {
+            $scope.projectInfo.requiredExpertise = "";
         }
 
-        $scope.updateProviderInfo = function() {
+        $scope.publishProject = function() {
+            console.log($scope.userName);
+            console.log($scope.projectInfo);
             $http({
                 method : 'POST',
-                url : '/users/'+ $scope.userName + '/providerinfo',
-                data: $scope.providerinfo
+                url : '/users/'+ $scope.userName + '/publishProject',
+                data: $scope.projectInfo
             }).success(function(data, status, headers, config) {
                 console.log(data);
-                $location.path('/dashboard');
+                $location.path('/showProject');
                 }
             ).error(function (data, status, headers, config) {
                 console.log(data);
             });
-        }
-
-        if($scope.role == 'serviceUser'){
-            // getUserInfo();
-        }
-        else{
-            $scope.getProviderInfo();
         }
     }
     editProfileController.$inject=['$scope', '$http', '$location', '$rootScope'];
