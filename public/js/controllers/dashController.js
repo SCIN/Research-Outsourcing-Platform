643dce5e63c5bed28d3c4f145eda61b56da60f68
SCIN <zhu.scin65536@gmail.com>
2016-11-17 19:46:12 -0800

    projects frontend

--- public/js/controllers/dashController.js
+++ public/js/controllers/dashController.js
@@ -1,59 +1,108 @@
 /*global define */
-define([], function() {
-	'use strict';
-	function dashController($scope, $http, $location, $rootScope){
+define([], function () {
+    'use strict';
+    function dashController($scope, $http, $location, $rootScope) {
         $scope.userName = $rootScope.user.userName;
         $scope.role = $rootScope.user.role;
         // console.log($rootScope.user.role);
         $scope.providerinfo = {
-            username : "",
+            username: "",
             credential: "",
-            researchAreas:"",
-            publications:"",
-            professionalServices:""
+            researchAreas: "",
+            publications: "",
+            professionalServices: ""
         };
 
         $scope.userinfo = {
-            keywords:""
+            username: "",
+            keywords: ""
         };
+        $scope.finishedProjects = [];
+        $scope.ongoingProjects = [];
 
-        $scope.getUserInfo = function() {
+        $scope.getUserInfo = function () {
             $http({
-                method : 'GET',
-                url : '/users/'+ $scope.userName + '/serviceuserinfo',
-            }).success(function(data, status, headers, config) {
+                method: 'GET',
+                url: '/users/' + $scope.userName + '/serviceuserinfo',
+            }).success(function (data, status, headers, config) {
                     $scope.userinfo = data;
-                     console.log(data);
+                    // console.log(data);
                 }
-
             ).error(function (data, status, headers, config) {
                 console.log(data);
             });
         }
 
-        $scope.getProviderInfo = function() {
+        $scope.getProviderInfo = function () {
             $http({
-                method : 'GET',
-                url : '/users/'+ $scope.userName + '/providerinfo'
-            }).success(function(data, status, headers, config) {
+                method: 'GET',
+                url: '/users/' + $scope.userName + '/providerinfo'
+            }).success(function (data, status, headers, config) {
                     $scope.providerinfo = data;
                 }
+            ).error(function (data, status, headers, config) {
+                console.log(data);
+            });
+        }
 
+        $scope.getProjectByPublisher = function () {
+            $http({
+                method: 'GET',
+                url: '/projects/publisher/' + $scope.userName
+            }).success(function (data, status, headers, config) {
+                    $scope.finishedProjects = [];
+                    $scope.ongoingProjects = [];
+                    $scope.$apply(function () {
+                        for (var i = 0; i < data.length; i++) {
+                            if (data[i].status != 'finished ') {
+                                $scope.finishedProjects.push(data[i]);
+                            }
+                            else {
+                                $scope.ongoingProjects.push(data[i]);
+                            }
+                        }
+                    })
+                }
             ).error(function (data, status, headers, config) {
                 console.log(data);
             });
         }
 
 
-
-        if($scope.role == 'serviceUser'){
-             $scope.getUserInfo();
+        $scope.getProjectByProvider = function () {
+            $http({
+                method: 'GET',
+                url: '/projects/provider/' + $scope.userName
+            }).success(function (data, status, headers, config) {
+                    $scope.finishedProjects = [];
+                    $scope.ongoingProjects = [];
+                    $scope.$apply(function () {
+                        for (var i = 0; i < data.length; i++) {
+                            if (data[i].status != 'finished ') {
+                                $scope.finishedProjects.push(data[i]);
+                            }
+                            else {
+                                $scope.ongoingProjects.push(data[i]);
+                            }
+                        }
+                    })
+                }
+            ).error(function (data, status, headers, config) {
+                console.log(data);
+            });
         }
-        else{
+
+        if ($scope.role == 'serviceUser') {
+            $scope.getUserInfo();
+            $scope.getProjectByPublisher();
+        }
+        else {
             $scope.getProviderInfo();
+            $scope.getProjectByProvider
         }
-	}
-	dashController.$inject=['$scope', '$http', '$location', '$rootScope'];
+    }
 
-	return dashController;
+    dashController.$inject = ['$scope', '$http', '$location', '$rootScope'];
+
+    return dashController;
 });
