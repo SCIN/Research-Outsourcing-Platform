4550e411959ab325b8a544e348168b26202f2528
SCIN <zhu.scin65536@gmail.com>
2016-11-30 23:47:47 -0800

    fix change status

--- public/js/controllers/ratingController.js
+++ public/js/controllers/ratingController.js
@@ -1,50 +1,25 @@
 /*global define */
 define([], function() {
     'use strict';
-    function editProfileController($scope, $http, $location, $rootScope){
-        console.log("rating controller");
+    function ratingController($scope, $http, $location, $rootScope){
         $scope.userName = $rootScope.user.userName;
         $scope.role = $rootScope.user.role;
         // console.log($rootScope.user.role);
 
-        // Mock Data: Test for all projects
-
-         $scope.allProjects =[];
-
-
-        $scope.getAllProjects = function() {
-            $http({
-                method : 'GET',
-                url : '/users/showProjects'
-            }).success(function(data, status, headers, config) {
-                    $scope.allProjects = data;
-                    console.log($scope.allProjects);
-                }
-
-            ).error(function (data, status, headers, config) {
-                 console.log(data);
-            });
-        }
-
-        $scope.provideProject = function(project){
-            $http({
-                method : 'POST',
-                url : '/projects/provide/'+$scope.userName,
-                data: {project:project}
-            }).success(function(data, status, headers, config) {
-                    $scope.getAllProjects();
-                    console.log(data);
-                }
-
-            ).error(function (data, status, headers, config) {
-                console.log(data);
-            });
-        }
+        $scope.rating ={
+            provider:'',
+            user:'',
+            project:'',
+            projectrate:5,
+            providerrate:5,
+            comment:''
+        };
+        
         if ($scope.role == 'serviceProvider'){
             $scope.getAllProjects();
         }
     }
-    editProfileController.$inject=['$scope', '$http', '$location', '$rootScope'];
+    ratingController.$inject=['$scope', '$http', '$location', '$rootScope'];
 
-    return editProfileController;
+    return ratingController;
 });
